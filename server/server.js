d671e81b0872769d5f4270cd72466b6f36462e04
Mariusz Nowak <medyk@medikoo.com>
2016-12-02 12:54:44 +0100

    Add port info

--- server/server.js
+++ server/server.js
@@ -58,5 +58,5 @@ module.exports = function (env) {
 	app.use(st({ path: staticsPath, cache: !env.dev, index: false, url: root }));
 	createServer(app).listen(port);
 
-	console.log("Server started");
+	console.log("Server started on", port, "port");
 };
